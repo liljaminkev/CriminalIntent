787412e51404db12bceeb9b7c1ab3d4927624c70
Kevin <kevchan85@yahoo.com>
2017-05-01 13:28:31 -0700

    List now populates with values from arrList
    Added:
    Solved Icon
    Logic to use solved icon if solved
    
    Modified Crimal:
    crimelistfragment

--- app/src/main/java/com/example/kevinchan/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/example/kevinchan/criminalintent/CrimeListFragment.java
@@ -7,7 +7,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -41,22 +43,31 @@ public class CrimeListFragment extends Fragment {
         mCrimeRecyclerView.setAdapter(mAdapter);
     }
 
-    private class CrimeHolder extends RecyclerView.ViewHolder{
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         private TextView mTitleTextView;
         private TextView mDateTextView;
         private Crime mCrime;
+        private ImageView mSolvedImageView;
 
         public CrimeHolder(LayoutInflater inflater, ViewGroup parent){
             super(inflater.inflate(R.layout.list_item_crime, parent, false));
+            itemView.setOnClickListener(this);
 
             mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
             mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+            mSolvedImageView = (ImageView) itemView.findViewById(R.id.crime_solved);
         }
 
         public void bind(Crime crime){
             mCrime = crime;
             mTitleTextView.setText(mCrime.getmTitle());
             mDateTextView.setText(mCrime.getmDate().toString());
+            mSolvedImageView.setVisibility(crime.ismSolved() ? View.VISIBLE : View.GONE);
+        }
+
+        @Override
+        public void onClick(View view){
+            Toast.makeText(getActivity(), mCrime.getmTitle() + " clicked!", Toast.LENGTH_SHORT).show();
         }
     }
 
